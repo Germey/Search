70f948c310c0ae7f7c6fe0f82183430839945579
Germey <cqc@cuiqingcai.com>
2017-03-18 16:39:12 +0800

    add bing

--- bing.py
+++ bing.py
@@ -6,45 +6,42 @@ import requests
 
 from proxy import get_proxy
 
-keyword = 'nba'
-
 headers = {
-    'Host': 'www.baidu.com',
+    'Host': 'cn.bing.com',
+    'Referer': 'http://cn.bing.com/',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Cookie': 'SRCHHPGUSR=CW=1231&CH=397&DPR=2&UTC=480&NEWWND=1&NRSLT=50&SRCHLANG=&AS=1&NNT=1&HIS=1&HAP=0'
 }
 
 
 def get_result(keyword, page):
     data = {
-        'wd': keyword,
-        'rn': 50,
-        'pn': (page - 1) * 50
+        'q': keyword,
+        'first': (page - 1) * 50,
     }
     proxy = get_proxy()
     proxies = {
         'https': 'https://' + proxy,
         'http': 'http://' + proxy
     }
-    url = 'https://www.baidu.com/s?' + urlencode(data)
+    url = 'http://cn.bing.com/search?' + urlencode(data)
     print(url)
     response = requests.get(url, headers=headers, proxies=proxies)
     if response.status_code == 200:
         html = response.text
         doc = pq(html)
-        results = doc('.result.c-container').items()
+        results = doc('li.b_algo').items()
         for result in results:
-            title = result.find('h3.t').text()
-            href = result.find('h3.t a').attr('href')
-            abstract = result.find('.c-abstract').text()
-            url = result.find('.c-showurl').text().replace(' ', '')
-            snapshot = result.find('.m').attr('href')
+            title = result.find('h2').text()
+            href = result.find('h2 a').attr('href')
+            abstract = result.find('.b_caption').text()
+            url = result.find('cite').text().replace(' ', '')
             yield dict({
                 'title': title,
                 'href': href,
                 'abstract': abstract,
                 'url': url,
-                'snapshot': snapshot
             })
 
 
--- web.py
+++ web.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 from flask import jsonify
 
-from baidu import get_request
+from baidu import get_request as request_baidu
+from bing import get_request as request_bing
 
 app = Flask(__name__)
 
@@ -10,7 +11,14 @@ def baidu():
     keyword = request.form['keyword']
     total = request.form['total']
     total = int(total)
-    return jsonify(get_request(keyword, total))
+    return jsonify(request_baidu(keyword, total))
+
+@app.route('/bing', methods=['POST'])
+def bing():
+    keyword = request.form['keyword']
+    total = request.form['total']
+    total = int(total)
+    return jsonify(request_bing(keyword, total))
 
 if __name__ == '__main__':
     app.run()
